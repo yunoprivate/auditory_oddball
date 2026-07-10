c3c4327ff5c5f878772f6b3f46d89c5f200c8818
yuno <amamiya.tobi.moon@gmail.com>
2026-07-10 12:34:15 +0900

    device chech

---
+++ test.py
@@ -0,0 +1,8 @@
+import psychotoolbox.audio as audio
+devices = audio.get_devices()
+
+print('=== Audio Output Devices ===')
+
+for device in devices:
+    if device['NrOutputChannels'] > 0:
+        print(f"{device['DeviceName']} ({device['DeviceID']})")
